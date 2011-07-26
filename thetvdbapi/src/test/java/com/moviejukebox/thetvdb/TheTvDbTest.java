a6b8c760affec276c66070deb5c28c115a190101
Omertron <Stuart.Boston@Gmail.com>
2011-07-26 14:50:36 +0000

    Added blank test cases

---
+++ thetvdbapi/src/test/java/com/moviejukebox/thetvdb/TheTvDbTest.java
@@ -0,0 +1,119 @@
+package com.moviejukebox.thetvdb;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TheTvDbTest {
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testTheTVDB() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetApiKey() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetLogger() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetLogger() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetProxy() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetTimeout() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetSeries() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetAllEpisodes() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetSeasonEpisodes() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetEpisode() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetDVDEpisode() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetAbsoluteEpisode() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetSeasonYear() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetBanners() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetActors() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSearchSeries() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetXmlMirror() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetBannerMirror() {
+        fail("Not yet implemented");
+    }
+
+}
