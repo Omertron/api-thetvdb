e7252a31b824884170e0c965d582ec5e4fcdb77b
Richard Miles <richard.miles@fast-chat.co.uk>
2014-12-27 17:19:18 +0000

    Thanks for making this, it's been really useful. I was reading the docs and it appears that the TVDB API now handles it's own load management, so there's no need to request mirrors.xml or retrieve one at random.
    http://thetvdb.com/wiki/index.php?title=API:mirrors.xml

--- src/main/java/com/omertron/thetvdbapi/model/Mirrors.java
+++ src/main/java/com/omertron/thetvdbapi/model/Mirrors.java
@@ -39,82 +39,13 @@ public class Mirrors {
     public static final String TYPE_BANNER = "BANNER";
     public static final String TYPE_ZIP = "ZIP";
 
-    private static final int MASK_XML = 1;
-    private static final int MASK_BANNER = 2;
-    private static final int MASK_ZIP = 4;
+    private static final String MIRROR_URL = "http://thetvdb.com/";
 
-    private static final Random RNDM = new Random();
-
-    private final List<String> xmlList = new ArrayList<String>();
-    private final List<String> bannerList = new ArrayList<String>();
-    private final List<String> zipList = new ArrayList<String>();
-
-    public Mirrors(String apiKey) {
-        // Make this synchronized so that only one
-        synchronized (this) {
-            String urlString = "http://www.thetvdb.com/api/" + apiKey + "/mirrors.xml";
-            Document doc;
-
-            doc = DOMHelper.getEventDocFromUrl(urlString);
-            int typeMask;
-            String url;
-
-            NodeList nlMirror = doc.getElementsByTagName("Mirror");
-            for (int nodeLoop = 0; nodeLoop < nlMirror.getLength(); nodeLoop++) {
-                Node nMirror = nlMirror.item(nodeLoop);
-
-                if (nMirror.getNodeType() == Node.ELEMENT_NODE) {
-                    Element eMirror = (Element) nMirror;
-                    url = DOMHelper.getValueFromElement(eMirror, "mirrorpath");
-                    typeMask = Integer.parseInt(DOMHelper.getValueFromElement(eMirror, "typemask"));
-                    addMirror(typeMask, url);
-                }
-            }
-        }
-    }
+    // Keep both constructors to maintain compatibility with existing code, but there's no need to supply the API key anymore
+    public Mirrors(){}
+    public Mirrors(String apiKey) {}
 
     public String getMirror(String type) {
-        String url = null;
-        if (type.equals(TYPE_XML) && !xmlList.isEmpty()) {
-            url = xmlList.get(RNDM.nextInt(xmlList.size()));
-        } else if (type.equals(TYPE_BANNER) && !bannerList.isEmpty()) {
-            url = bannerList.get(RNDM.nextInt(bannerList.size()));
-        } else if (type.equals(TYPE_ZIP) && !zipList.isEmpty()) {
-            url = zipList.get(RNDM.nextInt(zipList.size()));
-        }
-        return url;
-    }
-
-    private void addMirror(int typeMask, String url) {
-        switch (typeMask) {
-            case MASK_XML:
-                xmlList.add(url);
-                break;
-            case MASK_BANNER:
-                bannerList.add(url);
-                break;
-            case MASK_XML + MASK_BANNER:
-                xmlList.add(url);
-                bannerList.add(url);
-                break;
-            case MASK_ZIP:
-                zipList.add(url);
-                break;
-            case MASK_XML + MASK_ZIP:
-                xmlList.add(url);
-                zipList.add(url);
-                break;
-            case MASK_BANNER + MASK_ZIP:
-                bannerList.add(url);
-                zipList.add(url);
-                break;
-            case MASK_XML + MASK_BANNER + MASK_ZIP:
-                xmlList.add(url);
-                bannerList.add(url);
-                zipList.add(url);
-                break;
-            default:
-                break;
-        }
+        return MIRROR_URL;
     }
 }
